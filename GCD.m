a0cb4099cbff71768e6e27326bd106f03e811951
Mangesh Dharmal <mangeshdharmal@gmail.com>
2021-07-27 19:55:41 +0530

    change code

---
+++ GCD.m
@@ -0,0 +1,25 @@
+function GCD(m,n) 
+ %we listed all the factor of m
+ listm=[]
+   for i=1:m
+     if m/i - floor(m/i)==0
+     listm=[listm,i];
+     endif
+     
+   endfor
+   listn=[]
+ %\we listed all the factor of n
+    %for j=1:n
+     %if n/j - floor(n/j)==0
+     %listn=[listn,j]; 
+     %endif
+    %endfor
+    % c=[];
+   %for f = [listn]
+    % if sum(f == [listm]) == 1
+     %  c=[c,f];
+     %endif
+   %endfor
+   
+   %GCD = max(c)
+endfunction
